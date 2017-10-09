3e014df3d30a78490c7ce7d4500873de204ebeef
rodri090 <l2odri090@gmail.com>
2017-10-09 08:54:05 -0400

    actualizando

--- aplicacion1/src/pages/pagina4/pagina4.ts
+++ aplicacion1/src/pages/pagina4/pagina4.ts
@@ -23,11 +23,11 @@ export class Pagina4Page {
   nuevoRegistro = {};
   Registros: any=[{
     
-         Nombre:"jose",
-         Apellidos:"lopez",
-         CorreoElectronico:"jose@gmail.com",
-         Telefono:"1234567",
-         Direccion:"av.nn"
+         Nombre:"",
+         Apellidos:"",
+         CorreoElectronico:"",
+         Telefono:"",
+         Direccion:""
         
       }];
   
@@ -35,48 +35,12 @@ export class Pagina4Page {
     
   
    }
-    
-   /*alertaBasica(){
-    
-   let miAlerta= this.alerta.create({
-    title:'mi alerta',
-    message:'Registrese para hacer efectiva su reserva',
-    buttons:['Registrar']
-   });
-   
-   miAlerta.present()
-    //this.nuevoRegistro=this.data.load();
-    //this.data.saveData("hola");
-    //this.Registros=[
-     // {Nombre:"jose",Apellidos:"lopez",CorreoElectronico:"jose@gmail.com",Telefono:"1234567",Direccion:"av.nn"},
-      
-    //]
-   
-  }
- Registrar(){
-  this.Registros.push(this.nuevoRegistro);
-  this.nuevoRegistro = {};
- }
 
-  /*GuardarDatos(Nombre,Apellidos,CorreoElectronico,Telefono,Direccion ){
-  window.localStorage.setItem("Nombre", Nombre);
-  window.localStorage.setItem("Apellidos", Apellidos);
-  window.localStorage.setItem("Correo Electronico",CorreoElectronico);
-  window.localStorage.setItem("Telefono", Telefono);
-  window.localStorage.setItem("Direccion", Direccion);
-
- }
-
- ObtenerDatos(){
-
-  window.localStorage.getItem("Nombre");
-  window.localStorage.getItem("Apellidos");
-  window.localStorage.getItem("correo Elctronico");
-  window.localStorage.getItem("Telefono");
-  window.localStorage.getItem("Direccion");
-
-
-  }*/
-
+   Registrar(){
+     
+    this.Registros.push(this.nuevoRegistro);
+    this.nuevoRegistro = {};
+   }
+  
   
 }
